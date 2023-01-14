af3010d24e7f1c731e229f0207f02f226b77f0ee
Rafael Rollo <rafaelrollo92@gmail.com>
2023-01-14 19:57:17 +0000

    Refatora projeto para abandonar configuração do storyboard como base para a view

--- LearningTask-13.1/SceneDelegate.swift
+++ LearningTask-13.1/SceneDelegate.swift
@@ -19,14 +19,24 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(frame: scene.coordinateSpace.bounds)
+        window.windowScene = scene
+        
+        self.window = window
         
         let moviesAPI = MoviesAPI()
         let movieSessionsAPI = MovieSessionsAPI()
         
-        let homeController = (window!.rootViewController as! UINavigationController).topViewController as! MoviesViewController
+        let homeController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "HomeController") as! MoviesViewController
         homeController.moviesAPI = moviesAPI
         homeController.movieSessionsAPI = movieSessionsAPI
+        
+        let navigationController = UINavigationController(rootViewController: homeController)
+        
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
